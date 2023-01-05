28b2806f4252c27a1ac5efc10b79fc06c70453af
Epsilon <kumarsampurn@outlook.com>
2023-01-05 20:57:03 +0530

    Middleware added

--- client/script.js
+++ client/script.js
@@ -79,9 +79,34 @@ const handleSubmit = async(e)=>{
   const messageDiv =document.getElementById(uniqueId)
   loader(messageDiv)
 
-  
+  // fetch data from the server : bot's response
 
+  const response = await fetch('http://localhost:5000',{
+    method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
+    })
+  clearInterval(loadInterval);
+  messageDiv.innerHTML =""
+
+
+  if(response.ok){
+    const data = await response.json()
+    const parsedData = data.bot.trim()
+    console.log({parsedData})
+    typeText(messageDiv,parsedData)
+  }else{
+    const err = await response.text()
+    messageDiv.innerHTML="Something went Wrong"
+  }
 }
+
+
+
 
 
 form.addEventListener("submit",handleSubmit)
